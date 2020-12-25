4c8a5bcf1ae50445d6f210f4c1c7f561865901c1
Meet S. Patel <meet.s.patel.nj@gmail.com>
2020-12-25 17:44:38 -0500

    Base

---
+++ init.el
@@ -0,0 +1,17 @@
+(require 'package)
+(add-to-list 'package-archives (cons "melpa" "https://melpa.org/packages/") t)
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile (require 'use-package))
+
+(setq use-package-always-ensure t)
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
+
+(require 'org)
+(org-babel-load-file (expand-file-name (concat user-emacs-directory "settings.org")))
